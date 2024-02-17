03458ec88ad373bd5aa35cec7753124c39b76dc1
shahin2525 <shahin451999@gmail.com>
2024-02-17 13:25:34 -0800

    get all api added

--- index.js
+++ index.js
@@ -3,11 +3,59 @@ const app = express();
 const port = 3000;
 const cors = require("cors");
 
+app.use(cors());
+app.use(express.json());
+
+//
+
+// const { MongoClient, ServerApiVersion } = require("mongodb");
+// const uri =
+//   "mongodb+srv://job-portal-2:yObyukmtybQYzmPV@cluster0.ax6qyiu.mongodb.net/job-portal-3?retryWrites=true&w=majority";
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   },
+// });
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+
+//     // Send a ping to confirm a successful connection
+//     // await client.db("admin").command({ ping: 1 });
+//     const db = client.db("jobPort");
+//     const jobCollection = db.collection("jobs");
+
+//     app.post("/postJob", async (req, res) => {
+//       const data = req.body;
+//       const result = await jobCollection.insertOne(data);
+
+//       console.log("result", result);
+//       res.send(result);
+//     });
+
+//     console.log(
+//       "Pinged your deployment. You successfully connected to MongoDB!"
+//     );
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     // await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
+//
+
+const rakib = 12;
 //
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri =
-  "mongodb+srv://job-portal-2:XW3f9hmWre3FlGpM@cluster0.ax6qyiu.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://job-center:0jWlzsEnQ9qZF3OD@cluster0.ax6qyiu.mongodb.net/?retryWrites=true&w=majority";
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -16,13 +64,38 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
+
+    const dbName = client.db("job-center");
+    const jobCollection = dbName.collection("employments");
+
+    app.post("/postJob", async (req, res) => {
+      const data = req.body;
+      const result = await jobCollection.insertOne(data);
+
+      res.send(result);
+      console.log("result", result);
+    });
+
+    app.get("/allJobs/:text", async (req, res) => {
+      console.log(req.params.text);
+      if (req.params.text === "remote" || req.params.text === "offline") {
+        const result = await jobCollection
+          .find({ status: req.params.text })
+          .toArray();
+        return res.send(result);
+      }
+      const result = await jobCollection.find({}).toArray();
+      // console.log(result)
+      res.send(result);
+    });
+
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
@@ -35,7 +108,6 @@ run().catch(console.dir);
 
 //
 
-app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
